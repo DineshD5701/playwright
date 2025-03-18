0c967842940c162aba1e538846cb231492446dd2
Baldeep Singh <baldeep.singh@kapturecrm.com>
2025-03-18 11:31:56 +0530

    Created Login class for Meesho CX

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,35 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Meesho CX Login', async ({ page }) => {
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  // Go to meesho URL
+  await page.goto('https://meesho.kapturecrm.com/employee/index.html');
+
+  // Fill in the Username
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).type('testkap', {delay:100});
+
+  // Click on the Next button
+  await page.getByRole('button', { name: 'Next' }).click();
+
+  await page.waitForTimeout(1000)
+
+  // Fill the Password
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).type('Test@123', {delay:100});
+
+  // Click on Login
+  await page.getByRole('button', { name: 'Login' }).click();
+
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
