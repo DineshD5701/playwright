d2c4cda4cf5959433c20ac1c64bf70a6242e3070
Baldeep Singh <baldeep.singh@kapturecrm.com>
2025-04-02 17:58:04 +0530

    product locator changed

--- tests/meeshoCX.spec.js
+++ tests/meeshoCX.spec.js
@@ -229,7 +229,7 @@ test('Meesho CX', async ({ page }) => {
     await page.waitForTimeout(500)
 
     // Click on the Text Button
-    await page.getByText('116840453Product Id').click();
+    await page.getByRole('textbox').click();
     await page.waitForTimeout(1000)
 
     // Fill the text
