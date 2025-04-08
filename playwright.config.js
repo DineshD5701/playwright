127a8dad0a9a40501a718f39587a942aa9933b01
Baldeep Singh <baldeep.singh@kapturecrm.com>
2025-04-08 15:56:45 +0530

    timeout increased

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 60000, // Increase global test timeout to 60 seconds
+  timeout: 90000, // Increase global test timeout to 60 seconds
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
@@ -11,9 +11,9 @@ export default defineConfig({
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
-    timeout: 60000, // Ensure 60s timeout applies globally
-    actionTimeout: 20000, // 20s timeout for individual actions
-    navigationTimeout: 30000, // 30s timeout for page navigation
+    timeout: 90000, // Ensure 60s timeout applies globally
+    actionTimeout: 90000, // 20s timeout for individual actions
+    navigationTimeout: 90000, // 30s timeout for page navigation
   },
   projects: [
     {
