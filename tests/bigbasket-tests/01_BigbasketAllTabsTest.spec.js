0d97956f6f7d624df3f43c592af5af011a99fde1
DineshD5701 <dineshd3463@gmail.com>
2025-08-28 17:14:14 +0530

    test bb 1

--- tests/bigbasket-tests/01_BigbasketAllTabsTest.spec.js
+++ tests/bigbasket-tests/01_BigbasketAllTabsTest.spec.js
@@ -5,7 +5,7 @@ import TicketDetailspage from "../../Pages/TicketDetailsPage";
 const testdata = require("../../Generic/TestData.json");
 
 test("Bigbasket All Tabs Test", async ({ page }) => {
-  await page.goto(testdata["BigbasketNUIURL"], { waitUntil: "networkidle" });
+  //await page.goto(testdata["BigbasketNUIURL"], { waitUntil: "networkidle" });
   const ticketListPage = new TicketListPage(page);
   await ticketListPage.doAllTabsTest();
 });
