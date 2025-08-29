bceca6bdc0efae5acd1cd44c700ea35a01f9ad4d
DineshD5701 <dineshd3463@gmail.com>
2025-08-29 13:14:59 +0530

    increased timeout

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("@playwright/test");
 
 module.exports = defineConfig({
   testDir: "tests",
-  timeout: 60000,
+  timeout: 120000,
   retries: 0,
   reporter: [
     ["html"],
